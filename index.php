ff361a315e4abcd377ade70b5fa0b58878d21e74
Felipe-Daniel <felipedanielobmep2014@yahoo.com.br>
2021-07-14 21:56:38 -0300

    basic html and css done -> have to make multilayout footer and  nav

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet preload" href="style.css" as="style">
+</head>
+<body>
+    <main>
+        <nav>
+            <div class="logo">
+                Clubes Brasil
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Notícias</a></li>
+                <li><a href="#">Contatos</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+        <div class="news">
+            <div class="description"></div>
+            <div class="new">1</div>
+            <div class="new">2</div>
+            <div class="new">3</div>
+            <div class="new">4</div>
+            <div class="new">5</div>
+            <div class="new">6</div>
+            <div class="new">7</div>
+            <div class="new">8</div>
+            <div class="new">9</div>
+        </div>
+    </main>
+    <footer></footer>
+    
+</body>
+</html>
